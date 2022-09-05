467633da39ad9e06ba2267c1acf50aa5071503c9
Christian Wolf Johannsen <chwo@mapspeople.com>
2022-09-05 10:30:00 +0200

    Initial Commit

---
+++ mccloud/mccloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  mccloudApp.swift
+//  mccloud
+//
+//  Created by Christian Wolf Johannsen on 05/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct mccloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
